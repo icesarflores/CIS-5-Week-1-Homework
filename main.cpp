e278943ec7d96343f482985a91095e9042d99958
Cesar Flores <icesarflores@gmail.com>
2026-08-30 01:16:57 +0000

    Made output changes.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
   cin >> name;
   cout << "Years coding goal: ";
   cin >> years;
-  cout << name << ", " << "Sounds good. Your coding goal is " << "(" << years << ")\n";
+  cout << name << ", " << "Sounds good. Your coding goal is " << "(" << years << ")" << "." "\n";
   cout << "Hello World! I am " << name << ", and my coding goal is " << years << " years. Wish me luck!" << endl;
 
   return 0;
